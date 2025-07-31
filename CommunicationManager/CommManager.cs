43da0d9ee1126598bed13421a072e14d67eb5f7c
John Samarasinghe <John.Samarasinghe@mindoxtechno.com>
2025-07-31 06:26:38 +0530

    update

--- CommunicationManager/CommManager.cs
+++ CommunicationManager/CommManager.cs
@@ -11,6 +11,6 @@ public class CommManager
     public TelegramManager TBot { get; set; }
     public CommManager()
     {
-        // TBot = new("7887711227:AAE_mZUEnE5O7r8RN5j3kppe8D92b0Ir67Q");
+        
     }
 }
